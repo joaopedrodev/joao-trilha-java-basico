ad5009b6df685e42ed3d27da30a8aaad3ec8696d
João Pedro da Costa Vale <joaopedrodacostavale@gmail.com>
2023-03-14 21:27:23 -0300

    Projeto da conta bancária finalizada com sucesso !

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Agência! ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite a Agência!");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu saldo");
+        double saldo = scanner.nextDouble();
+
+         //imprimindo os dados obtidos pelo usuario
+         System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco  " + "sua agência é  " + agencia + "conta  " + numero + " e seu saldo " + saldo + " já esta disponível para saque ");
+         
     }
 }
